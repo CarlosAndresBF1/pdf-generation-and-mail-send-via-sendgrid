ffd93a6571c50d8bec76700cd1b3883ada04427f
Carlos Andres Beltran Franco <cbeltran@inmov.com>
2025-09-25 23:54:18 -0500

    fix: actualizar la versión del paquete a 2.0.1 en package.json

--- src/main.ts
+++ src/main.ts
@@ -33,7 +33,7 @@ async function bootstrap() {
   const config = new DocumentBuilder()
     .setTitle('Certificate Generation API')
     .setDescription('API for generating and managing certificates')
-    .setVersion('2.0.0')
+    .setVersion('2.0.1')
     .addBearerAuth(
       {
         type: 'http',
